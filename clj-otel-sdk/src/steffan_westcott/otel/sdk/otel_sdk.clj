46a2f9b233988cacf8f5065166200489a269b748
Steffan Westcott <steffan.westcott@gmail.com>
2021-11-08 22:48:35 +0000

    Refactor to please cljdoc
    More experiments to see if cljdoc will co-operate.

--- clj-otel-sdk/src/steffan_westcott/otel/sdk/otel_sdk.clj
+++ clj-otel-sdk/src/steffan_westcott/otel/sdk/otel_sdk.clj
@@ -18,7 +18,7 @@
   (if (instance? Resource resource)
     resource
     (let [{:keys [attributes schema-url]} resource]
-      (Resource/create (attr/->Attributes attributes) schema-url))))
+      (Resource/create (attr/->attributes attributes) schema-url))))
 
 (defn- merge-resources-with-default [resources]
   (reduce #(.merge ^Resource %1 (as-Resource %2)) (Resource/getDefault) resources))
