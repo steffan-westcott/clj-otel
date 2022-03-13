3e24e14e60ed6fa972f26a2e0575fabdf37f46ff
Steffan Westcott <steffan.westcott@gmail.com>
2022-03-13 14:45:02 +0000

    Release 0.1.1

--- build.clj
+++ build.clj
@@ -21,7 +21,7 @@
   "com.github.steffan-westcott")
 
 (def ^:private version
-  "0.1.1-SNAPSHOT")
+  "0.1.1")
 
 ;; Later artifacts in this vector may depend on earlier artifacts
 (def ^:private artifact-ids
