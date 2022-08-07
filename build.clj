f0652cfda8ccaf86b6d74ec340d97b15712308c0
Steffan Westcott <steffan.westcott@gmail.com>
2022-08-07 22:35:55 +0100

    Begin development of 0.1.5

--- build.clj
+++ build.clj
@@ -21,7 +21,7 @@
   "com.github.steffan-westcott")
 
 (def ^:private version
-  "0.1.4")
+  "0.1.5-SNAPSHOT")
 
 ;; Later artifacts in this vector may depend on earlier artifacts
 (def ^:private artifact-ids
