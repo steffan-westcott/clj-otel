54374150e97a6fd4fb86984287c56bb7c709e2fe
Steffan Westcott <steffan.westcott@gmail.com>
2022-11-19 17:10:49 +0000

    Bump OpenTelemetry deps to 1.20.1
    - Module `clj-otel-extension-aws` renamed to
     `clj-otel-contrib-aws-xray-propagator`

--- clj-otel-contrib-aws-xray-propagator/src/steffan_westcott/clj_otel/propagator/aws_xray.clj
+++ clj-otel-contrib-aws-xray-propagator/src/steffan_westcott/clj_otel/propagator/aws_xray.clj
@@ -1,7 +1,7 @@
 (ns steffan-westcott.clj-otel.propagator.aws-xray
   "Access to implementation of the [AWS X-Ray Trace Header propagation
   protocol](https://docs.aws.amazon.com/xray/latest/devguide/xray-concepts.html#xray-concepts-tracingheader)."
-  (:import (io.opentelemetry.extension.aws AwsXrayPropagator)))
+  (:import (io.opentelemetry.contrib.awsxray.propagator AwsXrayPropagator)))
 
 (defn aws-xray-propagator
   "Returns an implementation of the [AWS X-Ray Trace header propagation
