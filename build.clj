8e3bfb6029c1cfcafb95cc8e647e514625d0537b
Steffan Westcott <steffan.westcott@gmail.com>
2023-05-08 22:33:05 +0100

    Release 0.2.1

--- build.clj
+++ build.clj
@@ -21,7 +21,7 @@ clojure -A:deps -T:build help/doc"
   "com.github.steffan-westcott")
 
 (def ^:private version
-  "0.2.1-SNAPSHOT")
+  "0.2.1")
 
 ;; Later artifacts in this vector may depend on earlier artifacts
 (def ^:private artifact-ids
