7ead19eb1f8bfaaba230b328e6b00d252d5c6c57
Steffan Westcott <steffan.westcott@gmail.com>
2024-07-22 22:03:25 +0100

    Programmatic config of debug log record exporters

--- clj-otel-exporter-logging-otlp/src/steffan_westcott/clj_otel/exporter/logging_otlp.clj
+++ clj-otel-exporter-logging-otlp/src/steffan_westcott/clj_otel/exporter/logging_otlp.clj
@@ -1,21 +1,24 @@
 (ns steffan-westcott.clj-otel.exporter.logging-otlp
   "Exporters that log telemetry data in OTLP JSON format using
    `java.util.logging`. Intended for debugging only."
-  (:import (io.opentelemetry.exporter.logging.otlp OtlpJsonLoggingMetricExporter
+  (:import (io.opentelemetry.exporter.logging.otlp OtlpJsonLoggingLogRecordExporter
+                                                   OtlpJsonLoggingMetricExporter
                                                    OtlpJsonLoggingSpanExporter)
+           (io.opentelemetry.sdk.logs.export LogRecordExporter)
            (io.opentelemetry.sdk.metrics.data AggregationTemporality)
            (io.opentelemetry.sdk.metrics.export MetricExporter)
            (io.opentelemetry.sdk.trace.export SpanExporter)))
 
 (defn span-exporter
   "Returns a span exporter that logs every span in OTLP JSON format using
-   `java.util.logging`."
+   `java.util.logging`. Intended for debugging only."
   ^SpanExporter []
   (OtlpJsonLoggingSpanExporter/create))
 
 (defn metric-exporter
   "Returns a metric exporter that logs every metric in OTLP JSON format using
-   `java.util.logging`. May take an option map as follows:
+   `java.util.logging`. Intended for debugging only. May take an option map as
+   follows:
 
    | key                      | description |
    |--------------------------|-------------|
@@ -26,3 +29,9 @@
    [{:keys [aggregation-temporality]
      :or   {aggregation-temporality AggregationTemporality/CUMULATIVE}}]
    (OtlpJsonLoggingMetricExporter/create aggregation-temporality)))
+
+(defn log-record-exporter
+  "Returns a log record exporter that logs records in OTLP JSON format using
+   `java.util.logging`. Intended for debugging only."
+  ^LogRecordExporter []
+  (OtlpJsonLoggingLogRecordExporter/create))
--- clj-otel-exporter-logging/src/steffan_westcott/clj_otel/exporter/logging.clj
+++ clj-otel-exporter-logging/src/steffan_westcott/clj_otel/exporter/logging.clj
@@ -1,17 +1,20 @@
 (ns steffan-westcott.clj-otel.exporter.logging
   "Exporters that log telemetry data using `java.util.logging`. Intended for
    debugging only."
-  (:import (io.opentelemetry.exporter.logging LoggingMetricExporter LoggingSpanExporter)
+  (:import (io.opentelemetry.exporter.logging LoggingMetricExporter
+                                              LoggingSpanExporter
+                                              SystemOutLogRecordExporter)
            (io.opentelemetry.sdk.metrics.data AggregationTemporality)))
 
 (defn span-exporter
-  "Returns a span exporter that logs every span using `java.util.logging`."
+  "Returns a span exporter that logs every span using `java.util.logging`.
+   Intended for debugging only."
   ^LoggingSpanExporter []
   (LoggingSpanExporter/create))
 
 (defn metric-exporter
   "Returns a metric exporter that logs every metric using `java.util.logging`.
-   May take an option map as follows:
+   Intended for debugging only. May take an option map as follows:
 
    | key                      | description |
    |--------------------------|-------------|
@@ -22,3 +25,9 @@
    [{:keys [aggregation-temporality]
      :or   {aggregation-temporality AggregationTemporality/CUMULATIVE}}]
    (LoggingMetricExporter/create aggregation-temporality)))
+
+(defn log-record-exporter
+  "Returns a log record exporter that records to standard out. Intended for
+   debugging only."
+  ^SystemOutLogRecordExporter []
+  (SystemOutLogRecordExporter/create))
