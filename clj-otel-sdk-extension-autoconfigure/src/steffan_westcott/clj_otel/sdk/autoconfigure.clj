99a91a589ad2078270d25f1a51bdba261d182746
Steffan Westcott <steffan.westcott@gmail.com>
2025-10-10 22:37:46 +0100

    Docstring tweak

--- clj-otel-sdk-extension-autoconfigure/src/steffan_westcott/clj_otel/sdk/autoconfigure.clj
+++ clj-otel-sdk-extension-autoconfigure/src/steffan_westcott/clj_otel/sdk/autoconfigure.clj
@@ -24,25 +24,25 @@
    |`:set-as-default`            | If true, sets the configured SDK instance as the default `OpenTelemetry` instance declared and used by `clj-otel` (default: `true`).
    |`:set-as-global`             | If true, sets the configured SDK instance as the global `OpenTelemetry` instance declared by Java OpenTelemetry (default: `false`).
    |`:register-shutdown-hook`    | If true, registers a JVM shutdown hook to close the configured SDK instance (default: `true`).
-   |`:service-class-loader`      | ClassLoader to use for loading SPI implementations (default: same as for AutoConfiguredOpenTelemetrySdk class).
-   |`:component-loader`          | ComponentLoader to use for loading SPI implementations (default: same as for AutoConfiguredOpenTelemetrySdk class).
+   |`:service-class-loader`      | `ClassLoader` to use for loading SPI implementations (default: same as for `AutoConfiguredOpenTelemetrySdk` class).
+   |`:component-loader`          | `ComponentLoader` to use for loading SPI implementations (default: same as for `AutoConfiguredOpenTelemetrySdk` class).
    |                             |
-   |`:prop-overrides`            | fn that takes ConfigProperties and returns a string map of config property names and values to override defaults. Config property lookup precedence is: system property > env var > override > default.
-   |`:resource-fn`               | fn that takes Resource and ConfigProperties, and returns transformed Resource to use (default: no transform).
+   |`:prop-overrides`            | fn that takes `ConfigProperties` and returns a string map of config property names and values to override defaults. Config property lookup precedence is: system property > env var > override > default.
+   |`:resource-fn`               | fn that takes `Resource` and `ConfigProperties`, and returns transformed `Resource` to use (default: no transform).
    |                             |
-   |`:tracer-provider-builder-fn`| fn that takes SdkTracerProviderBuilder and ConfigProperties, and returns transformed SdkTracerProviderBuilder to use (default: no transform).
-   |`:span-exporter-fn`          | fn that takes SpanExporter and ConfigProperties, and returns transformed SpanExporter to use (default: no transform).
-   |`:span-processor-fn`         | fn that takes SpanProcessor and ConfigProperties, and returns transformed SpanProcessor to use (default: no transform).
-   |`:sampler-fn`                | fn that takes Sampler and ConfigProperties, and returns transformed Sampler to use (default: no transform).
-   |`:text-map-propagator-fn`    | fn that takes TextMapPropagator and ConfigProperties, and returns transformed TextMapPropagator to use (default: no transform).
+   |`:tracer-provider-builder-fn`| fn that takes `SdkTracerProviderBuilder` and `ConfigProperties`, and returns transformed `SdkTracerProviderBuilder` to use (default: no transform).
+   |`:span-exporter-fn`          | fn that takes `SpanExporter` and `ConfigProperties`, and returns transformed `SpanExporter` to use (default: no transform).
+   |`:span-processor-fn`         | fn that takes `SpanProcessor` and `ConfigProperties`, and returns transformed `SpanProcessor` to use (default: no transform).
+   |`:sampler-fn`                | fn that takes `Sampler` and `ConfigProperties`, and returns transformed `Sampler` to use (default: no transform).
+   |`:text-map-propagator-fn`    | fn that takes `TextMapPropagator` and `ConfigProperties`, and returns transformed `TextMapPropagator` to use (default: no transform).
    |                             |
-   |`:meter-provider-builder-fn` | fn that takes SdkMeterProviderBuilder and ConfigProperties, and returns transformed SdkMeterProviderBuilder to use (default: no transform).
-   |`:metric-exporter-fn`        | fn that takes MetricExporter and ConfigProperties, and returns transformed MetricExporter to use (default: no transform).
-   |`:metric-reader-fn`          | fn that takes MetricReader and ConfigProperties, and returns transformed MetricReader to use (default: no transform).
+   |`:meter-provider-builder-fn` | fn that takes `SdkMeterProviderBuilder` and `ConfigProperties`, and returns transformed `SdkMeterProviderBuilder` to use (default: no transform).
+   |`:metric-exporter-fn`        | fn that takes `MetricExporter` and `ConfigProperties`, and returns transformed `MetricExporter` to use (default: no transform).
+   |`:metric-reader-fn`          | fn that takes `MetricReader` and `ConfigProperties`, and returns transformed `MetricReader` to use (default: no transform).
    |                             |
-   |`:logger-provider-builder-fn`| fn that takes SdkLoggerProviderBuilder and ConfigProperties, and returns transformed SdkLoggerProviderBuilder to use (default: no transform).
-   |`:log-record-exporter-fn`    | fn that takes LogRecordExporter and ConfigProperties, and returns transformed LogRecordExporter to use (default: no transform).
-   |`:log-record-processor-fn`   | fn that takes LogRecordProcessor and ConfigProperties, and returns transformed LogRecordProcessor to use (default: no transform)."
+   |`:logger-provider-builder-fn`| fn that takes `SdkLoggerProviderBuilder` and `ConfigProperties`, and returns transformed `SdkLoggerProviderBuilder` to use (default: no transform).
+   |`:log-record-exporter-fn`    | fn that takes `LogRecordExporter` and `ConfigProperties`, and returns transformed `LogRecordExporter` to use (default: no transform).
+   |`:log-record-processor-fn`   | fn that takes `LogRecordProcessor` and `ConfigProperties`, and returns transformed `LogRecordProcessor` to use (default: no transform)."
   (^OpenTelemetrySdk [] (init-otel-sdk! {}))
   (^OpenTelemetrySdk
    [{:keys [set-as-default set-as-global register-shutdown-hook service-class-loader
